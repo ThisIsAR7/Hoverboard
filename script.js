4cee019447324ba232895af6b0f100cf0c973d9b
Arvin Rezaei <158085547+ThisIsAR7@users.noreply.github.com>
2024-08-23 17:57:46 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.getElementById('container')
-const colors = ['#e74c3c', '#8e44ad', '#'3498db, '#e67e22', '#2ecc71']
+const colors = ['#e74c3c', '#8e44ad', '#', '#e67e22', '#2ecc71']
 const SQUARES = 500
 
 for(let i = 0; i < SQUARES; i++) {
