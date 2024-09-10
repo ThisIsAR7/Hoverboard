31d8638c185aa215fffca510fe902e333f080dc4
AR7 | Arvin Rezaei <158085547+ThisIsAR7@users.noreply.github.com>
2024-09-10 09:02:46 +0330

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ for(let i = 0; i < SQUARES; i++) {
     container.appendChild(square)
 }
 
-function setColor(element) {
-   const color = getRandomColor()
+
+const color = getRandomColor()
    element.style.background = color
    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
