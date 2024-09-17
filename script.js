e4e3bc05e6813c532f642edbe5d53f63b392f468
AR7 | Arvin Rezaei <158085547+ThisIsAR7@users.noreply.github.com>
2024-09-17 23:41:49 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('container')
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
-const SQUARES = 500
+const SQUARES = 5
 
 for(let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div')
