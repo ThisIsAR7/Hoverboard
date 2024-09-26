888a31c602e8329b913fb2fd40f2c4e682a6d2ae
AR7 | Arvin Rezaei <158085547+ThisIsAR7@users.noreply.github.com>
2024-09-26 17:31:10 +0330

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ for(let i = 0; i < SQUARES; i++) {
 
 const color = getRandomColor()
    element.style.background = color
-   element.style.boxShadow = `0 0 2px ${}, 0 0 10px ${color}`
+   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
 function removeColor(element) {
